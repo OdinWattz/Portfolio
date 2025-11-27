0bcec31185360efdead8e30e6e9d456e71037682
OdinWattz <odinwattez@outlook.com>
2025-11-27 09:17:53 +0100

    hopefully fixed video's

--- src/once-ui/components/SmartImage.tsx
+++ src/once-ui/components/SmartImage.tsx
@@ -139,8 +139,12 @@ export const SmartImage: React.FC<SmartImageProps> = ({
               loop
               muted
               playsInline
+              preload="auto"
               style={{ width: "100%", height: "100%", objectFit }}
-            />
+            >
+              <source src={src} type="video/mp4" />
+              Your browser does not support the video tag.
+            </video>
           )}
           {!isLoading && isYouTube && (
             <iframe
